16b334a7203189ac1f51157ffb631d7da0f7ae71
wvanbreukelen <wdownload@xs4all.nl>
2017-01-15 00:08:50 +0100

    Added documentation

--- NFC.py
+++ NFC.py
@@ -26,9 +26,15 @@ class FormatError(Exception):
 
 class NFC:
     def __init__(self, ctl, tag_key):
+        # Instantie van Adafruit_PN532
         self.ctl = ctl
+
+        # Autorisatiecode voor NFC tag
         self.key = tag_key
 
+    """
+    Verbinding maken met de PN532 controller
+    """
     def begin(self):
         # Maak connectie met de PN532 controller
         self.ctl.begin()
@@ -36,15 +42,24 @@ class NFC:
         # Configuratie instellen op PN532
         self.ctl.SAM_configuration()
 
+    """
+    Zoek naar beschikbare NFC tags
+    """
     def scan(self):
         self.tag_id = self.ctl.read_passive_target()
 
+        # Als resultaat nul is, stuur false
         return self.tag_id != None
 
+    """
+    Lees datablock van NFC tag uit doormiddel een identificeerbare prefix
+    """
     def read(self, block, prefix, forcePrefix = True):
         if not self.authorizeBlock(block):
+            # Authorisatie mislukt, throw NFCAuthorizeException
             raise NFCAuthorizeException('Cannot authorize with NFC tag.')
 
+        # Data van block ophalen
         data = self.ctl.mifare_classic_read_block(block)
 
         if data is None:
@@ -63,11 +78,16 @@ class NFC:
         except Exception as e:
             raise FormatError('Format error on block {0} with data {1}.'.format(block, str(data)))
 
+        # Tuple bevat prefix met bijbehorende opgehaalde data
         return {prefix: fmtData}
 
+    """
+    Schrijf een datablock op het NFC tag. Gebruik prefix als identificatie van uniek datablock
+    """
     def write(self, block, prefix, input):
         # Gebruik sleutel om datasector vrij te geven voor schrijven
         if not self.authorizeBlock(block):
+            # Authorisatie mislukt, throw NFCAuthorizeException
             raise NFCAuthorizeException('Cannot authorize with NFC tag.')
 
         # Maak datapakket
@@ -78,6 +98,7 @@ class NFC:
 
         # Controleer of header juiste lengte heeft
         if prefixLen != 2:
+            # Header heeft verkeerde lengte, throw NFCPrefixMismatch exception
             raise NFCPrefixMismatch('Prefix "{0}" on block {1} has length of {2}, expecting 2.'.format(prefix, block, prefixLen))
 
         # Prefix header toevoegen aan eerste twee bytes. Op deze manier weten we dat deze rij het ID van de tag bevat
@@ -87,6 +108,7 @@ class NFC:
             # Zet input om naar hexadecimaal
             input = format(input, 'x')
         except Exception as e:
+            # Formatteerfout opgetreden, throw een FormatError exception
             raise FormatError('Cannot change input with type {0} to hexadecimal.'.format(type(input)))
 
         # Voeg nullen toe zodat de totale lengte op zes bytes komt
@@ -96,13 +118,17 @@ class NFC:
         # Plaats data in bytearray
         data[2:8] = input
 
-        # Schrijf data naar tag op block 1
+        # Schrijf data naar tag op block
         if not self.ctl.mifare_classic_write_block(block, data):
             raise NFCWriteError('Cannot write to NFC tag.')
 
+    """
+    Maak een datablock van een NFC tag schoon. Gebruik een prefix als identificatie
+    """
     def clean(self, block, prefix):
         # Gebruik sleutel om datasector vrij te geven voor schrijven
         if not self.authorizeBlock(block):
+            # Authorisatie mislukt, throw NFCAuthorizeException
             raise NFCAuthorizeException('Cannot authorize with NFC tag.')
 
         # Haal actuele data van het blok op
@@ -119,6 +145,9 @@ class NFC:
             # Schrijf block vol met lege bytearray
             self.ctl.mifare_classic_write_block(block, bytearray(16));
 
-    # Gebruik sleutel om datasector vrij te geven voor schrijven.
+    """
+    Autoriseer één datablock om te lezen/schrijven
+    """
     def authorizeBlock(self, block):
+        # Wanneer autorisatie gefaald heeft, stuur false
         return self.ctl.mifare_classic_authenticate_block(self.tag_id, block, PN532.MIFARE_CMD_AUTH_B, self.key)
